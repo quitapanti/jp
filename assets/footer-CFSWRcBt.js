c5e347ffe5bb9e918d13ed9108ec3028a5cc6bff
quitapanti <97330033+quitapanti@users.noreply.github.com>
2026-09-09 02:45:20 +0700

    Fix footer privacy-policy

--- assets/footer-CFSWRcBt.js
+++ assets/footer-CFSWRcBt.js
@@ -1,4 +1,4 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();class s extends HTMLElement{connectedCallback(){this.innerHTML=`
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();class s extends HTMLElement{connectedCallback(){this.innerHTML=`
         <footer>
           <ul class="links">
             <li>
@@ -47,7 +47,7 @@
               </a>
             </li>
           </ul>
-          <a href="privacy-policy.html" class="privacylink">terms and policy</a>
+          <a href="/privacy-policy/" class="privacylink">terms and policy</a>
           <p class="copyrightmark" style="font-size: small; color: var(--clr-accent-100)">
             Copyright &copy; 2026, Jimm Print. All Rights Reserved
           </p>
--- assets/main-Df2L7mIs.js
+++ assets/main-Df2L7mIs.js
@@ -1,4 +1,4 @@
-import"./footer-Cw45Beaw.js";/**
+import"./footer-CFSWRcBt.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
